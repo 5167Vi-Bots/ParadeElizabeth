e9186efa07cb650f3493921750feaa9d6a050735
Tommy <davidtklingerman@gmail.com>
2024-06-04 17:57:09 -0400

    Implemented needed methods for Quad Motor Controller Tank Chassis

--- shooter/QuadMotorTankChassis.cs
+++ shooter/QuadMotorTankChassis.cs
@@ -17,6 +17,29 @@ namespace shooter
             RightReverse = rightReverse;
         }
 
-        
+        public void SetSpeed(double ForwardReverse, double LeftRight)
+        {
+            LeftForward.SetSpeed(GetLeftSpeed(ForwardReverse, LeftRight));
+            LeftReverse.SetSpeed(GetLeftSpeed(ForwardReverse, LeftRight));
+            RightForward.SetSpeed(GetRightSpeed(ForwardReverse, LeftRight));
+            RightReverse.SetSpeed(GetRightSpeed(ForwardReverse, LeftRight));
+        }
+        public void Rotate(double DirectionSpeed)
+        {
+            if (DirectionSpeed > 0)
+            {
+                RightForward.SetSpeed(DirectionSpeed);
+                RightReverse.SetSpeed(DirectionSpeed);
+                LeftForward.SetSpeed(DirectionSpeed * -1);
+                LeftReverse.SetSpeed(DirectionSpeed * -1);
+            }
+            else
+            {
+                RightForward.SetSpeed(DirectionSpeed);
+                RightReverse.SetSpeed(DirectionSpeed);
+                LeftForward.SetSpeed(DirectionSpeed * -1);
+                LeftReverse.SetSpeed(DirectionSpeed * -1);
+            }
+        }
     }
 }
